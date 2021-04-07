26193093bb036620b4b537ca8ad3b3fcd7d36ae4
Tommaso Colella <tommasocolella95@gmail.com>
2021-04-07 18:48:26 +0200

    cleanup, add final recap table

--- analyser.py
+++ analyser.py
@@ -6,6 +6,7 @@ import argparse
 import subprocess
 from time import sleep
 import re
+import csv
 
 # uses the CLI interface in order to download a CID to the given path
 def get_cid():
@@ -22,7 +23,6 @@ def get_cid():
     process = subprocess.Popen(cmd, stdout=subprocess.DEVNULL)
     sleep(1)
     # poll the process in order to get the output
-    count = 1
 
     while True:
         if process.poll() is not None:
@@ -50,7 +50,7 @@ def bitswap_stat():
     return req.json()
 
 
-# get info on each peer's lefger from the bitswap
+# get info on each peer's ledger from the bitswap
 def ledger_info(peer):
     req = requests.post(
         "http://127.0.0.1:5001/api/v0/bitswap/ledger", params={"arg": peer}
@@ -60,26 +60,28 @@ def ledger_info(peer):
 
 # get info on the peer's ipfs node
 def id_info(contributors):
-    for peer in contributors:
+    print(f"Geolocating peers, ETA around {len(contributors)*1.5} seconds")
+    for count, peer in enumerate(contributors):
         req = requests.post(
             "http://127.0.0.1:5001/api/v0/id", params={"arg": peer["Peer"]}
         )
         resp = req.json()
-        address = extract_ip4(resp["Addresses"])
-        peer["Addr"] = address
+        addresses = extract_ip4s(resp["Addresses"])
+        peer["Addr"] = addresses
+        peer["Agent"] = resp["AgentVersion"]
 
         # try to geolocate the host
-        for addr in address:
-            country = geolocate(addr, polite=True)
+        for addr in addresses:
+            country = geolocate(addr)
             if country != "Unknown":
                 peer["Country"] = country
-        # assign unknown if no success
-        if country == "Unknown":
+        # assign unknown if no success (also if there are no addresses to check)
+        if country == "Unknown" or len(addresses) == 0:
             peer["Country"] = "Unknown"
 
 
 # extracts the first ip4 in order to geolocate the peer
-def extract_ip4(addresses):
+def extract_ip4s(addresses):
     ips = []
     pattern = r"((([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])[ (\[]?(\.|dot)[ )\]]?){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]))"
     for addr in addresses:
@@ -90,19 +92,50 @@ def extract_ip4(addresses):
     ip4s = set(ips)
     if "127.0.0.1" in ip4s:
         ip4s.remove("127.0.0.1")
-    print(ip4s)
     return list(ip4s)
 
 
+def get_latencies(contributors):
+    latencies = {}
+    req = requests.post("http://127.0.0.1:5001/api/v0/swarm/peers?latency=true")
+    resp = req.json()
+    # get the latencies from the response and put them into a dictionary
+    for peer in resp["Peers"]:
+        latencies[peer["Peer"]] = peer["Latency"]
+    # for each contributor, assign the correct latency
+    for contributor in contributors:
+        # [:-2] in order to remove the "ms" unit
+        if contributor["Peer"] in latencies:
+            latency = latencies[contributor["Peer"]][:-2]
+        else:
+            # placeholder value
+            latency = "Nan"
+        contributor["Latency"] = latency
+
+
+def print_infos(contributors, num_bytes):
+    print("\n--Recap on each Peer's contribution--")
+    print("----")
+
+    # print header
+    print(
+        f"{'Peer' : <10}{'PeerID' : ^10}{'Country' : ^20}{'Agent' : ^25}{'Latency(ms)' : ^15}{'RecvBytes' : ^10}{'Contrib%' : >5}"
+    )
+    for count, contributor in enumerate(contributors):
+        # print deadly table
+        print(
+            f"{count : <10}{contributor['Peer'][:5]+'...' : ^10}{contributor['Country'] : ^20}{contributor['Agent'][:22]+'...' : ^25}{contributor['Latency'] :^15.3f}{contributor['Recv'] : ^10}{contributor['Recv']/num_bytes*100:>5.3f}"
+        )
+
+
 def main():
-    print("test", args)
     get_cid()
     stats = bitswap_stat()
     peers = stats["Peers"]
     recv_blocks = stats["BlocksReceived"]
     recv_bytes = stats["DataReceived"]
 
-    print("--Bitswap stats regarding the transfer--")
+    print("\n--Bitswap stats regarding the transfer--")
     print(f"Received a total of {recv_blocks} blocks ({recv_bytes} bytes)")
     print(f"At the moment there are {len(peers)} peers in the swarm")
 
@@ -121,7 +154,26 @@ def main():
 
     # extract more info using ipfs id
     id_info(contributors)
-    print(contributors)
+
+    # get latencies from all the swarm and set the correct latency to each contributor
+    latencies = get_latencies(contributors)
+
+    # print final infos on each contributor
+    print_infos(contributors, recv_bytes)
+
+    # logging enabled
+    if args.l:
+        print("You can find more logs in the current directory")
+        # write logs
+        with open(
+            str(args.c) + "_peers.csv", "w", encoding="utf8", newline=""
+        ) as output_file:
+            fc = csv.DictWriter(
+                output_file,
+                fieldnames=contributors[0].keys(),
+            )
+            fc.writeheader()
+            fc.writerows(contributors)
 
 
 if __name__ == "__main__":
@@ -135,6 +187,11 @@ if __name__ == "__main__":
         help="The relative path where the CID will be stored. If not set, defaults to ./downloads/",
         type=str,
         default="./downloads/",
+    )
+    parser.add_argument(
+        "-l",
+        help="If set, logs statistics about the bitswap and the peers",
+        action="store_true",
     )
     args = parser.parse_args()
 
