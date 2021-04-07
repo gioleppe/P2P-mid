384a379e89e62bc9b353042b97e5f8fffa4b76d8
Tommaso Colella <tommasocolella95@gmail.com>
2021-04-07 18:48:08 +0200

    fixed prints

--- geolocator.py
+++ geolocator.py
@@ -2,10 +2,10 @@ import requests
 from time import sleep
 
 # geolocates an ip address returning its country as a string
-def geolocate(ip, polite):
+def geolocate(ip, polite=True):
+    if polite:
+        sleep(1.35)
     resp = requests.get("http://ip-api.com/json/" + ip).json()
     return resp["country"] if "country" in resp else "Unknown"
     # polite if we have more than 45 peers to geolocate
     # free api limited to 45 reqs/min
-    if polite:
-        sleep(1.35)
